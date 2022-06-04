da0017d094084476554b89fb857e0be1740083d2
Gerald Brose <broseg@htw-berlin.de>
2022-06-04 17:47:40 +0200

    Initial

---
+++ src/main/java/de/htw_berlin/imi/db/services/AbstractEntityService.java
@@ -0,0 +1,34 @@
+package de.htw_berlin.imi.db.services;
+
+import de.htw_berlin.imi.db.entities.Entity;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Base class for all entity services (DAOs).
+ *
+ * @param <E> the entity class.
+ */
+public abstract class AbstractEntityService<E extends Entity> {
+
+    @Value("${jdbc.connectionString}")
+    private String jdbcConnectionString;
+
+    protected ResultSet getResultSet(final String sql) throws SQLException {
+        return getStatement().executeQuery(sql);
+    }
+
+    private Statement getStatement() throws SQLException {
+        return DriverManager.getConnection(jdbcConnectionString).createStatement();
+    }
+
+    public abstract List<E> findAll();
+
+    public abstract Optional<E> findById(final long id);
+}
--- src/main/java/de/htw_berlin/imi/db/services/BueroRaumEntityService.java
+++ src/main/java/de/htw_berlin/imi/db/services/BueroRaumEntityService.java
@@ -2,13 +2,10 @@ package de.htw_berlin.imi.db.services;
 
 import de.htw_berlin.imi.db.entities.BueroRaum;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -18,13 +15,13 @@ import java.util.Optional;
  * <p>
  * Classes annotated with @Service can be injected using @Autowired in other Spring components.
  * <p>
- * Classes annotated with @Slf4j have access to loggers
+ * Classes annotated with @Slf4j have access to loggers.
  */
 @Service
 @Slf4j
-public class BueroRaumEntityService {
+public class BueroRaumEntityService extends AbstractEntityService<BueroRaum> {
 
-    private static final String ALL_BUEROS_SQL = """
+    private static final String FIND_ALL_QUERY = """
                 SELECT
                    id
                    ,name
@@ -35,41 +32,36 @@ public class BueroRaumEntityService {
                    ,stockwerk_id
                 FROM uni.v_bueros
             """;
-    @Value("${jdbc.connectionString}")
-    private String jdbcConnectionString;
 
+    private static final String FIND_BY_ID_QUERY = FIND_ALL_QUERY + " WHERE ID = ";
+
+    @Override
     public List<BueroRaum> findAll() {
         final List<BueroRaum> result = new ArrayList<>();
         try {
-            final ResultSet resultSet = getResultSet(ALL_BUEROS_SQL);
+            final ResultSet resultSet = getResultSet(FIND_ALL_QUERY);
             while (resultSet.next()) {
                 result.add(createBueroRaum(resultSet));
             }
         } catch (final Exception e) {
-            log.error("Problem finding bueros {}", e.getCause());
+            log.error("Problem finding bueros {}", e.getMessage());
         }
         return result;
     }
 
+    @Override
     public Optional<BueroRaum> findById(final long id) {
         try {
-            final ResultSet resultSet = getResultSet(ALL_BUEROS_SQL + " WHERE ID = " + id);
+            final ResultSet resultSet = getResultSet(FIND_BY_ID_QUERY + id);
             if (resultSet.next()) {
                 return Optional.of(createBueroRaum(resultSet));
             }
         } catch (final Exception e) {
-            log.error("Problem finding bueros {}", e.getCause());
+            log.error("Problem finding buero by id {}", e.getMessage());
         }
         return Optional.empty();
     }
 
-    private ResultSet getResultSet(final String sql) throws ClassNotFoundException, SQLException {
-        return getStatement().executeQuery(sql);
-    }
-
-    private Statement getStatement() throws ClassNotFoundException, SQLException {
-        return DriverManager.getConnection(jdbcConnectionString).createStatement();
-    }
 
     private BueroRaum createBueroRaum(final ResultSet resultSet) throws SQLException {
         final long id = resultSet.getInt("id");
